2ed3e8280acd434a7abdc6050facc826ada6f713
krypto <clayjeansen3@gmail.com>
2024-08-24 23:39:10 +0530

    update

---
+++ main.rb
@@ -0,0 +1,22 @@
+require_relative 'lib/tree'
+# arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+arr = [50,30,20,40,32,34,36,70,60,65,80,75,85]
+tree = Tree.new(arr)
+tree.eleminate_duplicate
+tree.build_tree
+puts tree.pretty_print
+
+tree.delete(50)
+tree.delete(36)
+tree.delete(80)
+# tree.delete(60)
+puts tree.pretty_print
+# tree.delete(4)
+# puts "\n"
+# puts tree.pretty_print
+
+# x = tree.root.left_child.data
+# y = tree.root.right_child.data
+# tree.root.left_child.data = 12
+# puts tree.pretty_print
+# puts x
